964d7b7695f4bc0a7e30cb8bed0d06b5a26f53e1
pallenJ <pallennellap17@gmail.com>
2019-11-25 10:23:05 +0900

    replymapper 메소드 추가

--- bootweb0/src/main/java/com/example/bootweb/mapper/ReplyMapper.java
+++ bootweb0/src/main/java/com/example/bootweb/mapper/ReplyMapper.java
@@ -1,5 +1,15 @@
 package com.example.bootweb.mapper;
 
-public interface ReplyMapper {
+import java.util.List;
 
+import com.example.bootweb.domain.ReplyVO;
+
+public interface ReplyMapper {
+	
+	public void insert(ReplyVO vo);
+	public ReplyVO detail(int rno); 
+	public List<?> list(int bno); 
+	public List<?> allList();
+	public int delete(int rno);
+	public void update(ReplyVO vo);
 }

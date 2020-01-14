99f4e716091a3756acfcb363ae56b30e1b2b2575
pallenJ <pallennellap17@gmail.com>
2020-01-15 07:48:40 +0900

    글 기록 레퍼지토리 작성

--- diary/src/main/java/com/pallen/diary/entity/board/Board_History.java
+++ diary/src/main/java/com/pallen/diary/entity/board/Board_History.java
@@ -1,5 +1,7 @@
 package com.pallen.diary.entity.board;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,17 +11,21 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.pallen.diary.entity.user.User;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "board_history")
 @Getter
+@Setter
 @AllArgsConstructor
 @Builder
 @ToString
@@ -34,4 +40,41 @@ public class Board_History {
 	@JoinColumn(name = "bno")
 	private Board original;
 
+	@Column(name = "board_kind")
+	private int board_kind;
+	@Column(name = "title")
+	private String title;
+	@Column(name = "content")
+	private String content;
+	//@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "reg_date")
+	private LocalDateTime reg_date;
+	//@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "update_date")
+	private LocalDateTime update_date;
+	@Column(name = "ip_address")
+	private String ip_address;
+	@Column(name = "state")
+	private String state;
+	@Column(name = "writer_email")
+	private String writer_email;
+	
+	public void setting(int board_kind, String title, String content, LocalDateTime reg_date,
+			LocalDateTime update_date, String ip_address, String state, User user) {
+		this.board_kind = board_kind;
+		this.title = title;
+		this.content = content;
+		this.reg_date = reg_date;
+		this.update_date = update_date;
+		this.ip_address = ip_address;
+		this.state = state;
+		this.writer_email = user.getEmail();
+	}
+	public void setting(Board brd) {
+		setting(brd.getBoard_kind(), brd.getTitle(), brd.getContent(), 
+				brd.getReg_date(), brd.getUpdate_date(), 
+				brd.getIp_address(), brd.getState(), brd.getUser());
+	}
+	
+	
 }
---
+++ diary/src/main/java/com/pallen/diary/entity/board/Board_HistoryRepository.java
@@ -0,0 +1,7 @@
+package com.pallen.diary.entity.board;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Board_HistoryRepository extends JpaRepository<Board_History, Long>{
+
+}

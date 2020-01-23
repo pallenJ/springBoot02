35bc7512e4c227ebee76918a17b3a25b5ae1633c
pallenJ <pallennellap17@gmail.com>
2020-01-23 19:21:48 +0900

    삭제 함수 추가

--- diary/src/main/java/com/pallen/diary/entity/board/BoardRepository.java
+++ diary/src/main/java/com/pallen/diary/entity/board/BoardRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.pallen.diary.entity.user.User;
 import com.pallen.diary.entity.board.Board;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import java.lang.Long;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long>{
 	List<Board> findByUser(User user);
 	List<Board> findByBno(Long bno);
@@ -21,6 +23,9 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
 	
 	@Query(value = "SELECT B FROM Board B WHERE B.state != ?1")
 	Page<Board> findByStateNot(@Param("state") String state,Pageable pageable);
-
+	
+	default void deleteByBno(long bno) {
+		delete(findByBno(bno).get(0));
+	}
 	
 }
--- diary/src/main/java/com/pallen/diary/entity/user/UserRepository.java
+++ diary/src/main/java/com/pallen/diary/entity/user/UserRepository.java
@@ -1,12 +1,14 @@
 package com.pallen.diary.entity.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.lang.String;
 import com.pallen.diary.entity.user.User;
 
 import java.util.List;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, String>{
 	List<User> findByEmail(String email);
 	default void deleteByEmail(String email) {

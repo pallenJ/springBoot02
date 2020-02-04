9e156b1639fb13afad93954836b742100e442386
pallenJ <pallennellap17@gmail.com>
2020-02-04 20:44:10 +0900

    카카오 로그인 완성

--- bootweb01/src/main/java/com/example/bootweb01/controller/HomeController.java
+++ bootweb01/src/main/java/com/example/bootweb01/controller/HomeController.java
@@ -1,10 +1,14 @@
 package com.example.bootweb01.controller;
 
+import java.util.Map;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.bootweb01.service.KakaoAPI;
@@ -23,10 +27,23 @@ public class HomeController {
 		return "index";
 	}
 	@GetMapping(value="/login")
-	public String login(@RequestParam("code") String code) {
-	    log.info("code : {}", code);
-	    String accessToken = kakaoAPI.getAccessToken(code);
-		log.info("controller access_token : {}", accessToken);
+	public String login(@RequestParam("code") String code, HttpSession session) {
+		 String access_Token = kakaoAPI.getAccessToken(code);
+		    Map<String, Object> userInfo = kakaoAPI.getUserInfo(access_Token);
+		    log.info("login Controller : {}" , userInfo);
+		    
+		    //    클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
+		    if (userInfo.get("email") != null) {
+		        session.setAttribute("userId", userInfo.get("email"));
+		        session.setAttribute("access_Token", access_Token);
+		    }
+		    return "index";
+	}
+	@RequestMapping(value="/logout")
+	public String logout(HttpSession session) {
+	    kakaoAPI.kakaoLogout((String)session.getAttribute("access_Token"));
+	    session.removeAttribute("access_Token");
+	    session.removeAttribute("userId");
 	    return "index";
 	}
 
--- bootweb01/src/main/java/com/example/bootweb01/service/KakaoAPI.java
+++ bootweb01/src/main/java/com/example/bootweb01/service/KakaoAPI.java
@@ -6,10 +6,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import lombok.extern.slf4j.Slf4j;
@@ -69,6 +72,77 @@ public class KakaoAPI {
 			// TODO: handle exception
 		}
 		return access_Token;
+	}
+
+	public Map<String, Object> getUserInfo(String access_Token) {
+	    //    요청하는 클라이언트마다 가진 정보가 다를 수 있기에 Map사용
+	    Map<String, Object> userInfo = new HashMap<>();
+	    String reqURL = "https://kapi.kakao.com/v2/user/me";
+	    try {
+	        URL url = new URL(reqURL);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("POST");
+	        
+	        //    요청에 필요한 Header에 포함될 내용
+	        conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+	        
+	        int responseCode = conn.getResponseCode();
+	        log.info("responseCode : {}" , responseCode);
+	        
+	        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	        
+	        String line = "";
+	        String result = "";
+	        StringBuffer rsSb = new StringBuffer();
+	        while ((line = br.readLine()) != null) {
+	            rsSb.append(line);
+	        }
+	        result = rsSb.toString();
+	        log.info("response body : {}" , result);
+	        
+	        JsonParser parser = new JsonParser();
+	        JsonElement element = parser.parse(result);
+	        
+	        JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+	        JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+	        
+	        String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+	        String email = kakao_account.getAsJsonObject().get("email").getAsString();
+	         
+	        userInfo.put("nickname", nickname);
+	        userInfo.put("email", email);
+	        
+	    } catch (Exception e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    }
+	    
+	    return userInfo;
+	}
+	public void kakaoLogout(String access_Token) {
+	    String reqURL = "https://kapi.kakao.com/v1/user/logout";
+	    try {
+	        URL url = new URL(reqURL);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("POST");
+	        conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+	        
+	        int responseCode = conn.getResponseCode();
+	        log.info("responseCode : {}" , responseCode);
+	        
+	        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	        
+	        String line = "";
+	        StringBuffer rsSb = new StringBuffer();
+	        while ((line = br.readLine()) != null) {
+	            rsSb.append(line);
+	        }
+	        String result = rsSb.toString();
+	        log.info(result);
+	    } catch (Exception e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    }
 	}
 
 }

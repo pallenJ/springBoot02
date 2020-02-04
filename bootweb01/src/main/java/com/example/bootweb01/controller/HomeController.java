eee2adbe864a74a123fca38c1d71a81ffc72386e
pallenJ <pallennellap17@gmail.com>
2020-02-04 11:45:59 +0900

    카톡 서비스토큰 받아오기 완료

--- bootweb01/src/main/java/com/example/bootweb01/controller/HomeController.java
+++ bootweb01/src/main/java/com/example/bootweb01/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.example.bootweb01.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.bootweb01.service.KakaoAPI;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,6 +15,9 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class HomeController {
 
+	@Autowired
+	private KakaoAPI kakaoAPI;
+	
 	@GetMapping(value = {"/", "/Main", "Home"})
 	public String index() {
 		return "index";
@@ -17,6 +25,8 @@ public class HomeController {
 	@GetMapping(value="/login")
 	public String login(@RequestParam("code") String code) {
 	    log.info("code : {}", code);
+	    String accessToken = kakaoAPI.getAccessToken(code);
+		log.info("controller access_token : {}", accessToken);
 	    return "index";
 	}
 
--- bootweb01/src/main/java/com/example/bootweb01/service/KakaoAPI.java
+++ bootweb01/src/main/java/com/example/bootweb01/service/KakaoAPI.java
@@ -1,10 +1,20 @@
 package com.example.bootweb01.service;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import org.springframework.stereotype.Service;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class KakaoAPI {
 
@@ -12,14 +22,49 @@ public class KakaoAPI {
 		String access_Token = "";
 		String refresh_Token = "";
 		String reqURL = "https://kauth.kakao.com/oauth/token";
-		
+
 		try {
 			URL url = new URL(reqURL);
-			HttpURLConnection conn= (HttpURLConnection)url.openConnection();
-			
-			//POST요청을 위해 setDoOutput을 true로
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+			// POST요청을 위해 setDoOutput을 true로
 			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+			StringBuffer sb = new StringBuffer();
+			// https://kauth.kakao.com/oauth/authorize?client_id=cd38b0930f04cc1506e2a3beecf1923c&redirect_uri=http://localhost:8007/login&response_type=code
+			sb.append("grant_type=authorization_code");
+			sb.append("&client_id=cd38b0930f04cc1506e2a3beecf1923c");
+			sb.append("&redirect_uri=http://localhost:8007/login");
+			sb.append("&code=" + authorize_code);
+			bw.write(sb.toString());
+			bw.flush();
+
+			int responseCode = conn.getResponseCode();
+			log.info("response code:{}", responseCode);
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = "";
+			StringBuffer rsSb = new StringBuffer();
+			String result = "";
 			
+			while ((line = br.readLine()) != null) {
+				rsSb.append(line);
+			}
+			result = rsSb.toString();
+			log.info("response body:{}", result);
+
+			//Json 파싱
+			
+			JsonParser parser = new JsonParser();
+			JsonElement element = parser.parse(result);
+			
+			access_Token  = element.getAsJsonObject().get("access_token").getAsString();
+			refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
+			
+			br.close();
+			bw.close();
 		} catch (Exception e) {
 			// TODO: handle exception
 		}

14bc7ded79ac74096d6afe5b3ac596331b69765e
pallenJ <pallennellap17@gmail.com>
2020-03-07 11:55:02 +0900

    boot에 시큐리티 설정실패. 좀더 자료를 찾아보거나 굳이 시큐리티를 안넣거나 둘중 하나.

--- bootweb02/src/main/java/com/example/demo/controller/HomeController.java
+++ bootweb02/src/main/java/com/example/demo/controller/HomeController.java
@@ -2,7 +2,11 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class HomeController {
 
@@ -10,6 +14,18 @@ public class HomeController {
 	public String main() {
 		return "index";
 	}
-	
-	
+	@GetMapping("/login")
+	public String login() {
+		log.info("login page");
+		return "/login";
+	}
+	@PostMapping("/login")
+	public String login(String username,String password) {
+		log.info("username:{}",username);
+		return "index";
+	}
+	@GetMapping("/admin")
+	public String admintest() {
+		return "/admin/admin_test";
+	}
 }
